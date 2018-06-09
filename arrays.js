768841d9af94775cdc87a155ac560020e71a11a6
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-09 22:42:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 function removeElementFromBeginningOfArray(arr){
   return arr.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  return arr.pop()
+}
