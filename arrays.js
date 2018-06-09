4efff3ba46661dbf7a671903659b7570a3e15976
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-09 22:37:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBar = ['snickers', 'hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray (arr, elem) {
+  return [elem, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  
+}
