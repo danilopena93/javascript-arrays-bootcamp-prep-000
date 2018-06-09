131d38a31a5f7e53225271f46693ed4cfb210cc3
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-09 22:44:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,37 @@
-var chocolateBar = ['snickers', 'hundred grand','kitkat','skittles']
+// var chocolateBar = ['snickers', 'hundred grand','kitkat','skittles']
 
-function addElementToBeginningOfArray (arr, elem) {
-  return [elem, ...arr]
-}
+// function addElementToBeginningOfArray (arr, elem) {
+//   return [elem, ...arr]
+// }
 
-function destructivelyAddElementToBeginningOfArray(arr, elem) {
-  return [elem, ...arr]
-}
+// function destructivelyAddElementToBeginningOfArray(arr, elem) {
+//   return [elem, ...arr]
+// }
 
-function addElementToEndOfArray (arr, elem) {
-  return [...arr, elem]
-}
+// function addElementToEndOfArray (arr, elem) {
+//   return [...arr, elem]
+// }
 
-function destructivelyAddElementToEndOfArray(arr, elem) {
-  return [...arr, elem]
-}
+// function destructivelyAddElementToEndOfArray(arr, elem) {
+//   return [...arr, elem]
+// }
 
-function accessElementInArray(arr, index){
-  return arr[index]
-}
+// function accessElementInArray(arr, index){
+//   return arr[index]
+// }
 
-function destructivelyRemoveElementFromBeginningOfArray(arr){
-  return arr.shift()
-}
+// function destructivelyRemoveElementFromBeginningOfArray(arr){
+//   return arr.shift()
+// }
 
-function removeElementFromBeginningOfArray(arr){
-  return arr.slice(1)
-}
+// function removeElementFromBeginningOfArray(arr){
+//   return arr.slice(1)
+// }
 
-function destructivelyRemoveElementFromEndOfArray(arr){
-  return arr.pop()
-}
+// function destructivelyRemoveElementFromEndOfArray(arr){
+//   return arr.pop()
+// }
 
-function removeElemetnFromEndOfArray(arr){
-  return arr.slice(0, arr.length-1)
-}
+// function removeElemetnFromEndOfArray(arr){
+//   return arr.slice(0, arr.length-1)
+// }
